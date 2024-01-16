32f34a54200acfb9909a239034a09b8a18485ca9
Sebastian Cyde <sebastian.cyde@red-engine.com>
2024-01-16 17:53:23 +0000

    Initial Grep Filtering

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -8,8 +8,41 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
 
+const filterScriptFileNames = async nameFilter => {
+	const allFiles = await vscode.workspace.findFiles();
+	const filteredFiles = allFiles.filter(
+		uri => uri.fsPath.includes(nameFilter) && uri.fsPath.includes("groovy")
+	);
+
+	// Get information about recently opened files
+	const recentlyOpened = vscode.window.state.recentlyOpened;
+
+	// Sort filteredFiles by most recently opened
+	filteredFiles.sort((a, b) => {
+		const aOpenedIndex = recentlyOpened.findIndex(
+			item => item.uri.fsPath === a.fsPath
+		);
+		const bOpenedIndex = recentlyOpened.findIndex(
+			item => item.uri.fsPath === b.fsPath
+		);
+
+		// Handle cases where a or b is not in the recentlyOpened array
+		if (aOpenedIndex === -1) return 1;
+		if (bOpenedIndex === -1) return -1;
+
+		// Sort by the order of opening
+		return bOpenedIndex - aOpenedIndex;
+	});
+
+	return filteredFiles.map(uri => ({
+		label: uri.fsPath.split("\\").slice(-3, -2).join("\\"),
+		description: uri.fsPath.split("Code\\").pop(),
+		fullPath: uri.fsPath,
+	}));
+};
+
+function activate(context) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "kat-nav" is now active!');
@@ -17,12 +50,15 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('kat-nav.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand(
+		"kat-nav.helloWorld",
+		function () {
+			// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from !');
-	});
+			// Display a message box to the user
+			vscode.window.showInformationMessage("Hello World from !");
+		}
+	);
 
 	context.subscriptions.push(disposable);
 }
@@ -32,5 +68,5 @@ function deactivate() {}
 
 module.exports = {
 	activate,
-	deactivate
-}
+	deactivate,
+};
